fd78f7ce9862d47780bb6b6424e6e822d5a14333
Mateus Rodrigues <matteus2204@gmail.com>
2026-05-20 21:22:56 -0300

    Exercício 3, solicita os valores de 20 filiais, exibe as lucrativas e calcula a média dos lucros apartir delas.

---
+++ Lista_de_Exer_Vetores_Matrizes/Ex3/Ex3.c
@@ -0,0 +1,27 @@
+/*3. Roberto é o dono de uma empresa com 20 filiais e precisa de ajuda para criar um algoritmo. Ele possui
+    um vetor com 20 valores reais, onde valores positivos representam filiais lucrativas e valores negativos
+    representam filiais que dão prejuízo. Você deve desenvolver um algoritmo que faça o seguinte:
+    (a) Imprima o índice no vetor de todas as empresas que dão lucro;
+    (b) Calcule a média dos lucros, desconsiderando todas as filiais que dão prejuízo.
+*/
+#include<stdio.h>
+
+int main() {
+	float filiais[20];
+	float media = 0;
+	int cont  = 0;
+
+	for(int i = 0; i<20; i++) {
+		printf("Digite o lucro da filial %d: ", i+1);
+		scanf("%f", &filiais[i]);
+	}
+	for(int j = 0; j<20; j++) {
+		if(filiais[j] > 0) {
+			printf("Empresas que dao lucro (filial %f): %f", filiais[j]);
+			media += filiais[j];
+			cont++;
+		}
+	}
+    media = media/cont;
+    printf("A média dos lucros é: %.2f", media);
+}
